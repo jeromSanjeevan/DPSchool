10415409be347d508627414ac2f0682cf31f6736
jeromsanjeevan <jerom6301@gmail.com>
2023-09-28 09:32:47 +0100

    First Demo Release

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,14 +1,15 @@
-import {  Component, OnInit,  } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
-  
-  constructor() { }
+  slideImage_1: string = 'assets/slider_images/1.jpeg';
+  slideImage_2: string = 'assets/slider_images/2.jpg';
+  slideImage_3: string = 'assets/slider_images/3.jpg';
+  constructor() {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 }
